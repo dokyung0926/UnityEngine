9d4b8273c888f148d8c2f9c3cf6dcc036fdfb546
Dokyeong <dlgnldhs0926@naver.com>
2022-02-18 21:57:14 +0900

    DiceGame
    움직이는 애니메이션 적용,
    기타 부가기능 설정 완료

--- RollADice/Assets/02.Scripts/DiceAnimationUI.cs
+++ RollADice/Assets/02.Scripts/DiceAnimationUI.cs
@@ -6,10 +6,27 @@ using UnityEngine.UI;
 
 public class DiceAnimationUI : MonoBehaviour
 {
+    public static DiceAnimationUI instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public bool isAvailable
+    {
+        get
+        {
+            bool isOK = false;
+            if (coroutine == null)
+                isOK = true;
+            return isOK;
+        }
+    }
     public Image diceAnimationImage;
     public float diceAnimationTime;
+    public GameObject diceAnimationFinishEffect;
 
     private List<Sprite> list_DiceImage = new List<Sprite> ();
+    Coroutine coroutine = null;
     private void Start()
     {
         LoadDiceImages();
@@ -23,5 +40,23 @@ public class DiceAnimationUI : MonoBehaviour
         {
             Debug.Log(item.name);
         }
+    }
+    public void PlayDiceAnimation(int diceValue)
+    {
+        coroutine = StartCoroutine(DiceAnimationCoroutin(diceValue));
+    } 
+    IEnumerator DiceAnimationCoroutin(int diceValue)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < diceAnimationTime)
+        {
+            elapsedTime += diceAnimationTime / 10;
+            int tmpIndex = Random.Range(0, list_DiceImage.Count);
+            diceAnimationImage.sprite = list_DiceImage[tmpIndex];
+            yield return new WaitForSeconds(diceAnimationTime / 10);
+        }
+        diceAnimationFinishEffect.SetActive(true);
+        diceAnimationImage.sprite = list_DiceImage[diceValue - 1];
+        coroutine = null;
     }
 }
